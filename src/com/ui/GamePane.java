6cb27e269a1ef278a114668126c5be162374a8d5
lindanpeng <lindanpeng@qq.com>
2016-09-30 11:34:41 +0800

    更新结构！

---
+++ src/com/ui/GamePane.java
@@ -0,0 +1,7 @@
+package com.ui;
+
+import javax.swing.JPanel;
+
+public class GamePane extends JPanel {
+
+}
---
+++ src/com/ui/StartPane.java
@@ -0,0 +1,7 @@
+package com.ui;
+
+import javax.swing.JPanel;
+
+public class StartPane extends JPanel {
+
+}
